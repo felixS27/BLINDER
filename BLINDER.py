3c56007f8e7477ad326892993c47641aacb6b4ab
Felix <135244625+felixS27@users.noreply.github.com>
2024-08-13 21:51:47 +0200

    Changed Progressbar to Text message

--- BLINDER.py
+++ BLINDER.py
@@ -18,19 +18,12 @@ def open_directory():
 def copy_and_rename(source_file,destination_file):
      shutil.copy(source_file,destination_file)
 
-def copy_and_rename_with_progress(src, dst, queue):
-    copy_and_rename(src, dst)
-    queue.put(1)
-
-def update_progress(queue):
-    while True:
-        progress = queue.get()
-        if progress is None:
-            break
-        progressbar['value'] += progress
-        root.update_idletasks()
+def update_status(message, color="black"):
+    status_label.config(text=message, fg=color)
+    root.update_idletasks()
 
 def blind_data():
+    update_status(message="Processing...",color="orange")
     source_path = Path(dir_path.get())
     destination_dir = source_path/'blinded'
     if not destination_dir.exists():
@@ -40,27 +33,21 @@ def blind_data():
     index=list(range(number_of_files))
     random.shuffle(index)
     file_name_mapping={f'{file.name}':f'{idx+1:03d}{file.suffix}' for file,idx in zip(files,index)}
-    queue = Queue()
-    thread = Thread(target=update_progress,args=(queue,))
-    thread.start()
-    progressbar['value'] = 0
-    progressbar['maximum'] = number_of_files
     with ThreadPoolExecutor() as executor:
-            futures = [executor.submit(copy_and_rename_with_progress,
+            futures = [executor.submit(copy_and_rename,
                                     source_path/original_file,
-                                    destination_dir/file_copy,
-                                    queue) for original_file,file_copy in file_name_mapping.items()]
+                                    destination_dir/file_copy) for original_file,file_copy in file_name_mapping.items()]
             for future in futures:
-                future.result() 
-    queue.put(None) #Stop progress updater
-    thread.join()
+                future.result()
+    update_status(message="Writing key file...",color="yellow")
     pd.DataFrame({'OriginalFile':[key for key in file_name_mapping.keys()],
         'BlindedFile':[value for value in file_name_mapping.values()]}).to_csv(destination_dir/'key.csv',index=False,sep='\t')
+    update_status(message="Finished!",color="green")
 
 root = tk.Tk()  # create parent window
 root.title('BLINDER - Blinding files for data analysis') # set window title
-root.minsize(500,200) # set minimal window size
-root.geometry('500x200+550+300') # set window size and position
+root.minsize(300,200) # set minimal window size
+root.geometry('300x200+550+300') # set window size and position
 
 # define entry point for directory path
 tk.Label(root,text='File directory').grid(row=0,column=0,padx=5,pady=5,sticky='E')
@@ -78,9 +65,9 @@ file_suffix.grid(row=1,column=1,padx=5, pady=5)
 tk.Button(root,text='Start Blinding Data!',command=blind_data,
           relief=tk.RAISED).grid(row=2,column=1,padx=5, pady=5)
 
-#define progressbar
-progressbar = ttk.Progressbar(orient=tk.HORIZONTAL,length=200,mode='determinate')
-progressbar.grid(row=3,column=0,columnspan=3,padx=5,pady=5)
+# define status label
+status_label = tk.Label(root, text="Wait for action.")
+status_label.grid(row=3,column=1,padx=5, pady=5)
 
 # define button for ending program
 tk.Button(root,text='End blinding.',command=root.quit,
