1ba86f92c9d6b86412c8a88c2326e197051cf9e4
Felix <135244625+felixS27@users.noreply.github.com>
2024-09-19 21:05:33 +0200

    Add various control statements

--- BLINDER.py
+++ BLINDER.py
@@ -7,21 +7,92 @@ import shutil
 from concurrent.futures import ThreadPoolExecutor
 import pandas as pd
 import webbrowser
+from datetime import datetime
 
-def open_directory():
-    directory=filedialog.askdirectory()
-    if directory:
-        dir_path.delete(0,tk.END)
-        dir_path.insert(0,directory)
+# toplevel function
+def blind_data():
+    check_label_filling()
+
+# check for correct usage (all fields are filled out correctly)
+def check_label_filling():
+    source_path = Path(dir_path.get())
+    if source_path.exists() and len(str(source_path))>1:
+        if len(file_suffix.get())>0:
+            if file_suffix.get().startswith("."):
+                files = sorted([file for file in source_path.glob(f'*{file_suffix.get()}')])
+                number_of_files = len(files)
+                if number_of_files > 0:
+                    check_blinded_folder(source_path,files,number_of_files)
+                else:
+                    open_errormessage(f'Directory does not contain files with {file_suffix.get()} suffix.',0)
+            else:
+                open_errormessage("File suffix does not start with a \".\"",0)
+        else:
+            open_errormessage("No file suffix provided.",0)
+    else:
+        open_errormessage("File directory does not exist.",0)
+
+# Check if blinded data already exists
+def check_blinded_folder(source_path,files,number_of_files):
+    blinded_path = source_path/'blinded'
+    if blinded_path.exists():
+        file_list = [file for file in blinded_path.glob(f'*{file_suffix.get()}')]
+        if len(file_list)>0:
+            print('Blinded folder contains blinded data.')
+            open_errormessage("The folder contains already blinded data.",1)
+        else:
+            key_path = blinded_path/'key.csv'
+            if key_path.exists():
+                open_errormessage("The folder contains a \"key.csv\" file.",1)
+            else:
+                blind_files(append=False)
+    else:
+        blind_files(append=False)
+
 
 def copy_and_rename(source_file,destination_file):
-     shutil.copy(source_file,destination_file)
+    shutil.copy(source_file,destination_file)
 
 def update_status(message, color="black"):
     status_label.config(text=message, fg=color)
     root.update_idletasks()
 
-def blind_data():
+def update_counter(count, total):
+    counter_label.config(text=f"Processed {count} out of {total} files")
+    root.update_idletasks()
+
+def open_errormessage(message,stage):
+    errorwindow = tk.Toplevel(root)
+    errorwindow.title("Warning!")
+    errorwindow.focus()
+    errorwindow.grab_set()
+    
+    if stage==0:
+        # A Label widget to show in toplevel
+        errorwindow.geometry('250x150+550+300')
+        tk.Label(errorwindow,text="Blinding not possible!").grid(row=0,column=0,padx=5, pady=5)
+        tk.Label(errorwindow,text=message).grid(row=1,column=0,padx=5, pady=5)
+        tk.Label(errorwindow,text="Please fix the error and restart blinding.").grid(row=2,column=0,padx=5, pady=5)
+        tk.Button(errorwindow, text="Close", command=errorwindow.destroy).grid(row=3,column=0,padx=5, pady=5)
+
+    elif stage==1:
+        errorwindow.geometry('270x180+550+300')
+        for i in range(3):  # Assuming there are 3 buttons
+            errorwindow.grid_columnconfigure(i, minsize=10, weight=0)
+        tk.Label(errorwindow,text="Directory already contains a blinded folder!").grid(row=0,column=0,columnspan=3,padx=5, pady=5)
+        tk.Label(errorwindow,text=message).grid(row=1,column=0,columnspan=3,padx=5, pady=5)
+        tk.Label(errorwindow,text="Please choose if you want to overwrite the files,\nappend new files or abort blinding.").grid(row=2,column=0,columnspan=3,padx=5, pady=5)
+        tk.Button(errorwindow, text="Overwrite", command=lambda: blinding(errorwindow,append=False)).grid(row=3,column=0,padx=5, pady=5,sticky='E')
+        tk.Button(errorwindow, text="Append", command=lambda: blinding(errorwindow,append=True)).grid(row=3,column=1,padx=5, pady=5)
+        tk.Button(errorwindow, text="Abort", command=errorwindow.destroy).grid(row=3,column=2,padx=5, pady=5,sticky='W')
+        tk.Button(errorwindow, text="Close", command=errorwindow.destroy).grid(row=4,column=1,padx=5, pady=5)
+
+
+def blinding(top_window,append):
+    top_window.destroy()
+    blind_files(append=append)
+
+def blind_files(append=False):
     source_path = Path(dir_path.get())
     destination_dir = source_path/'blinded'
     if not destination_dir.exists():
@@ -30,10 +101,23 @@ def blind_data():
     number_of_files = len(files)
     index=list(range(number_of_files))
     random.shuffle(index)
-    file_name_mapping={f'{file.name}':f'{idx+1:03d}{file.suffix}' for file,idx in zip(files,index)}
-    pd.DataFrame({'OriginalFile':[key for key in file_name_mapping.keys()],
-                  'BlindedFile':[value for value in file_name_mapping.values()]}).to_csv(destination_dir/'key.csv',
-                                                                                         index=False,sep='\t')
+    if append:
+        no_blinded_files = len([file for file in destination_dir.glob(f'*{file_suffix.get()}')])
+    if append:
+        file_name_mapping={f'{file.name}':f'{idx+1+no_blinded_files:03d}{file.suffix}' for file,idx in zip(files,index)}
+    else:
+        file_name_mapping={f'{file.name}':f'{idx+1:03d}{file.suffix}' for file,idx in zip(files,index)}
+    if append:
+        old_key = pd.read_csv(destination_dir/'key.csv',sep='\t')
+        new_key = pd.DataFrame({'OriginalFile':[key for key in file_name_mapping.keys()],
+                                'BlindedFile':[value for value in file_name_mapping.values()],
+                                'Date':datetime.now().strftime("%Y-%m-%d_%H-%M-%S")})
+        pd.concat([old_key,new_key], ignore_index=True).to_csv(destination_dir/'key.csv',index=False,sep='\t')
+    else:
+        pd.DataFrame({'OriginalFile':[key for key in file_name_mapping.keys()],
+                    'BlindedFile':[value for value in file_name_mapping.values()],
+                    'Date':datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}).to_csv(destination_dir/'key.csv',index=False,sep='\t')
+    
     update_status(message="Processing...",color="orange")
     processed_files = 0
     update_counter(processed_files, number_of_files)
@@ -46,21 +130,24 @@ def blind_data():
                 processed_files += 1
                 update_counter(processed_files, number_of_files)
     update_status(message="Finished!",color="green")
+    
 
-def open_webpage():
-    webbrowser.open("https://github.com/felixS27/BLINDER")
-
-def update_counter(count, total):
-    counter_label.config(text=f"Processed {count} out of {total} files")
-    root.update_idletasks()
-
+# ### GUI
+# ##Setup general window
 root = tk.Tk()  # create parent window
 root.title('BLINDER - Blinding files for data analysis') # set window title
 root.minsize(300,200) # set minimal window size
 root.geometry('500x250+550+300') # set window size and position
 ttk.Style().theme_use('default') # set window style/theme
 
+# ##Directory entry label
 # define entry point for directory path
+def open_directory():
+    directory=filedialog.askdirectory()
+    if directory:
+        dir_path.delete(0,tk.END)
+        dir_path.insert(0,directory)
+# actual label 
 tk.Label(root,text='File directory').grid(row=0,column=0,padx=5,pady=5,sticky='E')
 dir_path=tk.Entry(root,bd=1,relief=tk.FLAT)
 dir_path.grid(row=0,column=1,padx=5, pady=5)
@@ -88,7 +175,11 @@ counter_label.grid(row=4,column=1,padx=5, pady=5)
 tk.Button(root,text='Close BLINDER.',command=root.quit,
           relief=tk.RAISED).grid(row=5,column=2,padx=5,pady=5)
 
+# ##Button for link to GitHub page
 # define the help/about button directing to the BLINDER GitHub page
+def open_webpage():
+    webbrowser.open("https://github.com/felixS27/BLINDER")
+# actual button
 tk.Button(root, text="Help/About", command=open_webpage,
           relief=tk.RAISED).grid(row=5,column=0,padx=5,pady=5) 
 
@@ -97,7 +188,7 @@ tk.Label(root, text="Powered by Felix Schneider",
          font=("Arial", 9, "italic")).grid(row=5,column=1,padx=5,pady=5) 
 
 # add version number
-tk.Label(root, text="Version 0.2.2",
+tk.Label(root, text="Version 0.3.0",
          font=("Arial", 9, "italic")).grid(row=6,column=1,padx=5,pady=5) 
 
 root.mainloop()
